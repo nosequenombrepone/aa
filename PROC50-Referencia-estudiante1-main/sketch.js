325b5e8e9608cf36c736dfc3bad2cbb68f7eff52
nosequenombrepone <142558237+nosequenombrepone@users.noreply.github.com>
2024-06-13 15:54:09 -0600

    Add files via upload

---
+++ PROC50-Referencia-estudiante1-main/sketch.js
@@ -0,0 +1,76 @@
+var bg, bgImg
+var bottomGround
+var topGround
+var balloon, balloonImg,gameOver,gameOverImage
+
+
+function preload(){
+bgImg = loadImage("assets/bg.png")
+
+
+balloonImg = loadAnimation("assets/balloon1.png","assets/balloon2.png","assets/balloon3.png")
+gameOverImage=loadImage("assets/gameOver.png")
+}
+
+
+function setup(){
+
+
+//Imagen de fondo
+bg = createSprite(165,485,1,1);
+bg.addImage(bgImg);
+bg.scale = 1.3
+
+
+//Crear bases superiores e inferiores
+bottomGround = createSprite(200,390,800,20);
+bottomGround.visible = false;
+
+
+topGround = createSprite(200,10,800,20);
+topGround.visible = false;
+     
+//Crear globo      
+balloon = createSprite(100,200,20,50);
+balloon.addAnimation("balloon",balloonImg);
+balloon.scale = 0.2;
+
+
+gameOver=createSprite(200,200,200,50)
+gameOver.addImage(gameOverImage)
+gameOver.visible=false
+
+
+
+
+
+
+}
+
+
+function draw() {
+ 
+  background("black");
+       
+          //Hacer brincar el globo aerostático
+          if(keyDown("space")) {
+            balloon.velocityY= -6 ;
+           
+          }
+
+
+          //Añadir gravedad
+           balloon.velocityY = balloon.velocityY+2;
+          if(balloon.position.y>400){
+            gameOver.visible=true;
+          }
+
+
+   
+        drawSprites();
+       
+}
+
+
+
+
